88e6110c17a82bbc7c1f5c95b4112a93e59b4e02
XicorNguyen <xicor267@gmail.com>
2023-04-13 15:20:57 +0700

    User-SubAdmin

---
+++ frontend/src/components/dashborad/AdminInfo.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux'
+import {logout_user} from '../../store/actions/authAction'
+
+
+const AdminInfo = ({ profileModelShow, userInfo }) => {
+
+  const dispatch = useDispatch()
+
+  const history = useHistory()
+
+  const logout = () => {
+    dispatch(logout_user({ role: userInfo.role, history }))
+  }
+
+  return (
+    <div className={`adminInfo ${profileModelShow ? 'show' : ''}`}>
+      <div className="image-email">
+        <img src={userInfo.image} alt="" />
+        <span>{userInfo.email}</span>
+      </div>
+      <ul>
+        <li><Link to='/dashborad/sub-admin-profile/323'>Profile</Link></li>
+        <li><Link to='/'>View site</Link></li>
+        <li onClick={logout}><span>Logout</span></li>
+      </ul>
+    </div>
+  )
+}
+
+export default AdminInfo
